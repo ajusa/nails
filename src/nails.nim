4db79076a397f01ed3d23157aae3394a2087bc41
Arham Jain <arhamjain@gmail.com>
2023-07-30 20:25:09 -0700

    actually push autofill code

--- src/nails.nim
+++ src/nails.nim
@@ -1,4 +1,4 @@
-import mummy, webby, std/[cookies, strtabs]
+import mummy, webby, std/[cookies, strtabs, sequtils, strutils]
 export strtabs, webby
 
 type Response* = object
@@ -11,32 +11,79 @@ proc query*(req: Request): QueryParams =
 proc cookies*(req: Request): StringTableRef =
   req.headers["Cookie"].parseCookies
 
-proc getOrDefault*(params: QueryParams, key, default: string): string =
-  if key in params: params[key] else: default
-
 proc redirect*(req: Request, path: string) =
   var headers: HttpHeaders
   headers["Location"] = path
   req.respond(302, headers)
 
-template wire*(module: untyped, meth: untyped): untyped =
-  block:
-    proc handler(req: Request) =
-      var headers: HttpHeaders
-      headers["Content-Type"] = "text/html"
-      var response = Response(headers: headers, code: 200)
-      when compiles(module.meth(req)):
-        when not (type(module.meth(req)) is void):
-          let tmp = module.meth(req)
-        else:
-          module.meth(req)
-      when compiles(tmp.meth):
-        let resp = tmp.meth
-        if not req.responded:
-          req.respond(response.code, response.headers, resp)
-      when compiles(meth()):
-        let resp = meth
-        if not req.responded:
-          req.respond(response.code, response.headers, resp)
-    handler
+proc respond*(req: Request, body: string) =
+  req.respond(200, @[("Content-Type", "text/html")], body)
 
+func params*(req: Request): QueryParams =
+  concat(req.query, req.body.parseSearch).QueryParams
+
+proc parseParam*(params: QueryParams; k: string; v: var SomeInteger)
+proc parseParam*(params: QueryParams; k: string; v: var SomeFloat)
+proc parseParam*(params: QueryParams; k: string; v: var bool)
+proc parseParam*(params: QueryParams; k: string; v: var string)
+proc parseParam*[T](params: QueryParams; k: string; v: var seq[T])
+proc parseParam*[T: object|tuple](params: QueryParams, k: string, v: var T)
+
+proc fillArgs*[T](
+  handler: proc(req: Request, args: T) {.gcsafe.}): RequestHandler =
+  mixin fromRequest
+  proc(req: Request) =
+    var args: T
+    req.fromRequest(args)
+    req.handler(args)
+
+proc fillArgs*[T](
+  handler: proc(req: Request, args: var T) {.gcsafe.}): RequestHandler =
+  mixin fromRequest
+  proc(req: Request) =
+    var args: T
+    req.fromRequest(args)
+    req.handler(args)
+
+proc parseParam*(params: QueryParams; k: string; v: var SomeInteger) =
+  v = params[k].parseInt
+
+proc parseParam*(params: QueryParams; k: string; v: var SomeFloat) =
+  v = params[k].parseFloat
+
+proc parseParam*(params: QueryParams; k: string; v: var bool) =
+  v = k in params
+
+proc parseParam*(params: QueryParams; k: string; v: var string) =
+  v = params[k]
+
+proc parseParam*[T](params: QueryParams; k: string; v: var seq[T]) =
+  for i, pair in params:
+    if pair[0] == k:
+      var el: T
+      parseParam(params.toBase[i..^1].QueryParams, k, el)
+      v.add(el)
+
+proc parseParam*[T: object|tuple](params: QueryParams, k: string, v: var T) =
+  for key, value in v.fieldPairs:
+    params.parseParam(key, value)
+
+proc fromRequest*[T: object|tuple](req: Request, v: var T) =
+  mixin fromRequest
+  for key, value in v.fieldPairs:
+    when compiles(req.fromRequest(value)):
+      req.fromRequest(value)
+    else:
+      let params = req.params
+      params.parseParam(key, value)
+
+proc fromRequest*[T: ref object](req: Request; v: var T) =
+  mixin fromRequest
+  let params = req.params
+  when compiles(new(v)):
+    new(v)
+  for key, value in v[].fieldPairs:
+    when compiles(req.fromRequest(value)):
+      req.fromRequest(value)
+    else:
+      params.parseParam(key, value)
